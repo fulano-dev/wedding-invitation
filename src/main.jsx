5d53cfdac0da6494c5a9ef8abe44e9c1cce24d9d
Joao Pedro <joao@fulano.dev>
2025-03-25 18:34:38 -0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -15,12 +15,15 @@
  */
 
 import { StrictMode } from 'react'
+import { HashRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>,
 )
